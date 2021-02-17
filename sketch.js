d0f41d01680337ecdbb98f9fe4903f2eece0872e
parassha <77490755+parassha@users.noreply.github.com>
2021-02-17 20:40:09 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,80 +1,114 @@
 const Engine = Matter.Engine;
-const World= Matter.World;
+const World = Matter.World;
 const Bodies = Matter.Bodies;
+const Body = Matter.Body;
 const Constraint = Matter.Constraint;
 
-var engine, world;
-var box1, pig1;
-var backgroundImg,platform;
-var bird, slingShot;
 
-function preload() {
-    backgroundImg = loadImage("sprites/bg.png");
+function setup() {
+  createCanvas(1200,600);
+ 
+
+  engine = Engine.create();
+  world = engine.world;
+
+  Engine.run(engine)
+
+  ground = new Ground(600,580,1200,20);
+
+  ground2 = new Ground(390,300,260,10);
+  ground3 = new Ground(900,400,200,10);
+
+  polygon = new Polygon(150,200,30,30);
+
+  launcher = new Slingshot(polygon.body,{x:150, y:110})
+  
+  
+  block1 = new Block(480,275,30,40);
+  block2 = new Block(450,275,30,40);
+  block3 = new Block(420,275,30,40);
+  block4 = new Block(390,275,30,40);
+  block5 = new Block(360,275,30,40);
+  block6 = new Block(330,275,30,40);
+  block7 = new Block(300,275,30,40);
+  block8 = new Block(330,235,30,40);
+  block9 = new Block(360,235,30,40);
+  block10 = new Block(390,235,30,40);
+  block11 = new Block(420,235,30,40);
+  block12 = new Block(450,235,30,40);
+  block13 = new Block(360,195,30,40);
+  block14 = new Block(390,195,30,40);
+  block15 = new Block(420,195,30,40);
+  block16 = new Block(390,155,30,40);
+
+  b1 = new Block(900,275,30,40);
+  b2 = new Block(900,315,30,40);
+  b3 = new Block(870,315,30,40);
+  b4 = new Block(930,315,30,40);
+  b5 = new Block(900,355,30,40);
+  b6 = new Block(870,355,30,40);
+  b7 = new Block(840,355,30,40);
+  b8 = new Block(930,355,30,40);
+  b9 = new Block(960,355,30,40);
+  
+
+
 }
 
-function setup(){
-    var canvas = createCanvas(1200,400);
-    engine = Engine.create();
-    world = engine.world;
+function draw() {
+  background(55,43,43);
+  ground.display();
+  ground2.display();
+  ground3.display();
 
+  block1.display();
+  block2.display();
+  block3.display();
+  block4.display();
+  block5.display();
+  block6.display();
+  block7.display();
+  block8.display();
+  block9.display();
+  block10.display();
+  block11.display();
+  block12.display();
+  block13.display();
+  block14.display();
+  block15.display();
+  block16.display(); 
 
-    ground = new Ground(600,height,1200,20);
-    platform = new Ground(150, 305, 300, 170);
+  b1.display();
+  b2.display();
+  b3.display();
+  b4.display();
+  b5.display();
+  b6.display();
+  b7.display();
+  b8.display();
+  b9.display();
 
-    box1 = new Box(700,320,70,70);
-    box2 = new Box(920,320,70,70);
-    pig1 = new Pig(810, 350);
-    log1 = new Log(810,180,300, PI/2);
+  polygon.display();
 
-    box3 = new Box(700,240,70,70);
-    box4 = new Box(920,240,70,70);
-   // pig3 = new Pig(810, 220);
-
-   // log3 =  new Log(810,180,300, PI/2);
-
-    box5 = new Box(810,160,70,70);
-    //log4 = new Log(760,120,150, PI/7);
-    //log5 = new Log(870,120,150, -PI/7);
-
-    bird = new Bird(200,50);
-    birdn = new Bird(100,100);
-
-    //log6 = new Log(230,180,80, PI/2);
-    slingshot = new SlingShot(bird.body,{x:200, y:50});
+  launcher.display();
+  drawSprites();
 }
 
-function draw(){
-    background(backgroundImg);
-    Engine.update(engine);
-    strokeWeight(4);
-    box1.display();
-    box2.display();
-    ground.display();
-    pig1.display();
-    log1.display();
-
-    box3.display();
-    box4.display();
-   // pig3.display();
-   // log3.display();
-
-    box5.display();
-   // log4.display();
-   // log5.display();
-
-    bird.display();
-    
-    platform.display();
-    //log6.display();
-    slingshot.display();    
-   
+function mouseDragged()  {
+  Matter.Body.setPosition(polygon.body,{x: mouseX, y: mouseY})
 }
 
-function mouseDragged(){
-    Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
+function mouseReleased()  {
+  launcher.fly()
+  
+
 }
 
+function keyPressed()  {
+  if(keyCode===32)  {
+    Matter.Body.setPosition(polygon.body,{x:150, y:200})
+    launcher.attach(polygon.body);
 
-function mouseReleased(){
-    slingshot.fly();
+  }
+
 }
